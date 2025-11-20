2c390cde0ac6f2c0d50ccb42779f1e247401b907
Calvin Grabowski <grabowca@flip2.engr.oregonstate.edu>
2025-11-19 23:03:25 -0800

    most of the moods made and set, only a bit left, and then more optimizing and what not

--- myproject-env/data_set/pull_playlists.py
+++ myproject-env/data_set/pull_playlists.py
@@ -8,30 +8,82 @@ def classify_mood(row):
     valence = row['valence']
     energy = row['energy']
     danceability = row['danceability']
-    acousticness = row['acousticness']
-    loudness = row['loudness']
+    loudness = (row['loudness'])/60 + 1
     speechiness = row['speechiness']
+    tempo = (row['tempo'])/200
+    
 
-    if valence > 0.7 and energy > 0.6:
-        return 'Happy'
-    elif valence < 0.3 and energy < 0.5:
-        return 'Sad'
-    elif acousticness > 0.5 and energy < 0.6 and valence > 0.4:
-        return 'Chill'
-    elif valence < 0.3 and energy > 0.7 and loudness < -5:
-        return 'Angry'
-    elif 0.4 < valence < 0.7 and energy < 0.5 and acousticness > 0.4:
-        return 'Romantic'
-    elif energy > 0.8 and danceability > 0.7 and loudness > -6:
-        return 'Hype'
+    # this normalizes the ranges to be more like 0-1
+
+    acousticness = row['acousticness'] # probably least helpful
+
+   
+
+    if (acousticness > 0.85 and danceability < 0.5) :
+        mood = "Instrumental"
+    
+    elif ((valence > 0.6 and valence < 0.75 and energy > 0.6 and tempo < 0.6)):
+        mood = 'Happy'
+    elif (valence > 0.6 and speechiness < 0.07 and danceability < 0.7):
+        mood = 'Strumming' # guitar could be considered hype subclass of it
+    elif ((valence > 0.1 and valence < 0.6) and (danceability > 0.4 and danceability < 0.8) and (loudness > 0.8) and (speechiness > 0.03 and speechiness < 0.06) and (tempo > 0.5 and tempo < 0.8)):
+        mood = 'Hopeful'
+    elif (loudness > 0.8 and danceability > 0.3 and valence > 0.2) or energy > 0.9:
+        if (tempo > 0.6 and valence > 0.3) :
+            mood = 'Hype'
+        else :
+            mood = 'Slow Hype'
+    elif (valence < 0.4 and danceability < 0.5 and tempo > 0.7):
+        mood = "Atmospheric Chill"
+    elif ((valence < 0.4 and danceability < 0.5 and speechiness < 0.05) or (energy < 0.6 and danceability < 0.6 and tempo < 0.7)):
+        if acousticness > 0.5:
+            mood = 'Extra Chill'
+        else:
+            mood = 'Chill'
+    elif (valence > 0.65 and danceability > 0.65 and loudness > 0.85 and tempo < 0.65):
+        mood = 'Upbeat Chill'   # fast, chill, yet groovy
+    elif ((valence < 0.5 and (energy < 0.5 or danceability < 0.5)and tempo < 0.6)):
+        if speechiness > 0.06:
+            mood = 'Slow Rap'
+        else:
+            mood = "Slow"
+    elif (acousticness > 0.85 or (danceability > 0.5 and valence > 0.5)):
+        mood = "From the Heart"
+    elif (danceability > 0.85) :
+        mood = "Club Beats"
+    
+    # elif () :
+    #     mood = 'Unknown'
+    
+    # elif valence < 0.4 and energy > 0.7 and loudness > -6 and speechiness > 0.4:
+    #     mood = 'Angry'
+    # elif 0.5 <= valence <= 0.7 and energy < 0.5 and speechiness < 0.4:
+    #     mood = 'Romantic'
+    # elif valence > 0.6 and energy > 0.8:
+    #     mood = 'Excited'
+    # elif valence < 0.4 and energy < 0.3 and speechiness < 0.3:
+    #     mood = 'Lonely'
     else:
-        return 'Neutral'
+        mood = 'Unknown'
+    return mood 
+
 
 # Apply mood classification
 df['mood'] = df.apply(classify_mood, axis=1)
+df['loudness'] = df['loudness']/60 + 1
+df['tempo'] = df['tempo']/200
 
 # Save to new file
 df.to_csv('updated_mood_data.csv', index=False)
 
+specific_songs = df[df['mood'] == 'Unknown']
+
+# Then take a random sample of 50
+random_specific_50 = specific_songs
+# figure out how preprocess the data
+print(random_specific_50[['track_name', 'track_artist', 'valence', 'danceability', 'loudness', 'speechiness', 'tempo', 'mood', 'acousticness']])
+
+# 'track_name', 'track_artist', 'valence', 'energy', 'mood'
 # Preview
-print(df[['track_name', 'track_artist', 'valence', 'energy', 'mood']].head())
+print(df[['track_name', 'track_artist', 'valence', 'danceability', 'loudness', 'speechiness', 'tempo', 'mood']].sample(50))
+print(df[df['track_name'] == "Disease"])
